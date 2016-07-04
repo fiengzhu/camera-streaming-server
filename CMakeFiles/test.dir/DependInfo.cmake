59da4b847c58ca9092a423cde7ec0d38236bd6dd
fiengzhu <feingzhu@hotmail.com>
2016-07-04 13:53:46 -0400

    New

---
+++ CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zzz/h3d/camera-server/newtry/src/VideoStreamingController.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/VideoStreamingController.cpp.o"
+  "/home/zzz/h3d/camera-server/newtry/src/VideoStreamingRequestHandler.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/VideoStreamingRequestHandler.cpp.o"
+  "/home/zzz/h3d/camera-server/newtry/src/VideoStreamingRequestHandlerFactory.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/VideoStreamingRequestHandlerFactory.cpp.o"
+  "/home/zzz/h3d/camera-server/newtry/src/VideoStreamingServer.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/VideoStreamingServer.cpp.o"
+  "/home/zzz/h3d/camera-server/newtry/src/WebCamController.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/WebCamController.cpp.o"
+  "/home/zzz/h3d/camera-server/newtry/src/WebcamService.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/WebcamService.cpp.o"
+  "/home/zzz/h3d/camera-server/newtry/src/main.cpp" "/home/zzz/h3d/camera-server/newtry/CMakeFiles/test.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/src/kernel/include/uapi"
+  "/usr/src/kernel/include"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/main.cpp.o"
+  "CMakeFiles/test.dir/src/VideoStreamingServer.cpp.o"
+  "CMakeFiles/test.dir/src/VideoStreamingRequestHandlerFactory.cpp.o"
+  "CMakeFiles/test.dir/src/VideoStreamingRequestHandler.cpp.o"
+  "CMakeFiles/test.dir/src/VideoStreamingController.cpp.o"
+  "CMakeFiles/test.dir/src/WebCamController.cpp.o"
+  "CMakeFiles/test.dir/src/WebcamService.cpp.o"
+  "test.pdb"
+  "test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
